7bbbd15af4f62a65d8ac18d524a026c89d2007a7
rafu01 <nrafsan13@gmail.com>
2020-09-05 22:50:19 +0600

    Updates

---
+++ precache-manifest.96a4c92d222c703c55cb8262976da001.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab7aaf4dd4fca3c3b1c2f76082c51739",
+    "url": "/bd-covid19-predictor/index.html"
+  },
+  {
+    "revision": "8bc689eaaac74cef2110",
+    "url": "/bd-covid19-predictor/static/css/main.d2e9b150.chunk.css"
+  },
+  {
+    "revision": "a3c4f67edb3dcaebdd4d",
+    "url": "/bd-covid19-predictor/static/js/2.d39544d2.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/bd-covid19-predictor/static/js/2.d39544d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bc689eaaac74cef2110",
+    "url": "/bd-covid19-predictor/static/js/main.c97f13aa.chunk.js"
+  },
+  {
+    "revision": "c4401b5baa4bb57e6244",
+    "url": "/bd-covid19-predictor/static/js/runtime-main.45c2c5a7.js"
+  },
+  {
+    "revision": "a2a26edad53050b0ca948f0ea3975f3b",
+    "url": "/bd-covid19-predictor/static/media/corona.a2a26eda.png"
+  }
+]);
